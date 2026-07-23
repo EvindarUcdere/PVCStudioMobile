39407a68fd8f967a76964bfbdb5e5f1b694363f4
EvindarUcdere <ucdereevindar34@gmail.com>
2026-07-23 14:24:58 +0300

    Fix jobs detail route

--- app/jobs.tsx
+++
@@ -1,5 +0,0 @@
-import { JobsScreen } from '../src/features/jobs/screens/JobsScreen';
-
-export default function JobsRoute() {
-  return <JobsScreen />;
-}
---
+++ app/jobs/index.tsx
@@ -0,0 +1,5 @@
+import { JobsScreen } from '../../src/features/jobs/screens/JobsScreen';
+
+export default function JobsRoute() {
+  return <JobsScreen />;
+}
